03cfe1067ef7f0e892dff77b2a1a35c6b4ab0be3
Daniel Lehman <dlehman@esri.com>
2018-08-17 10:17:28 -0700

    softpipe: Advertise GL_ARB_texture_buffer_range.
    base the size on the block size of the largest format
    
    Signed-off-by: Daniel Lehman <dlehman@esri.com>

--- src/gallium/drivers/softpipe/sp_screen.c
+++ src/gallium/drivers/softpipe/sp_screen.c
@@ -186,7 +186,7 @@ softpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_MAX_TEXTURE_BUFFER_SIZE:
       return 65536;
    case PIPE_CAP_TEXTURE_BUFFER_OFFSET_ALIGNMENT:
-      return 0;
+      return util_format_get_blocksize(PIPE_FORMAT_R64G64B64A64_FLOAT);
    case PIPE_CAP_TGSI_TEXCOORD:
    case PIPE_CAP_PREFER_BLIT_BASED_TEXTURE_TRANSFER:
       return 0;
