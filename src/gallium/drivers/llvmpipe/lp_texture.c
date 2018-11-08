6fcc12be2f5d378170ff79cd4143b8b785fc5a7e
Daniel Lehman <dlehman@esri.com>
2018-11-07 17:44:15 -0800

    llvmpipe: remove TODO about align_malloc
    it's not redundant
    align_malloc is about the returned pointer, not the size
    
    duh

--- src/gallium/drivers/llvmpipe/lp_texture.c
+++ src/gallium/drivers/llvmpipe/lp_texture.c
@@ -170,7 +170,7 @@ llvmpipe_texture_layout(struct llvmpipe_screen *screen,
 
    if (allocate) {
       unsigned nr_samples = MAX2(pt->nr_samples, 1);
-      lpr->tex_data = align_malloc(lpr->sample_size * nr_samples, mip_align); /* TODO: align_malloc is redundant */
+      lpr->tex_data = align_malloc(lpr->sample_size * nr_samples, mip_align);
       if (!lpr->tex_data) {
          return FALSE;
       }
