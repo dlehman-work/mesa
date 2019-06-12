4c5bd632201f168679bad5d87b7018d897950d95
Daniel Lehman <dlehman@esri.com>
2019-06-12 10:51:18 -0700

    stub STORE

--- src/gallium/auxiliary/gallivm/lp_bld_tgsi_soa.c
+++ src/gallium/auxiliary/gallivm/lp_bld_tgsi_soa.c
@@ -3460,6 +3460,19 @@ load_emit(
     */
 }
 
+static void
+store_emit(
+   const struct lp_build_tgsi_action * action,
+   struct lp_build_tgsi_context * bld_base,
+   struct lp_build_emit_data * emit_data)
+{
+   struct lp_build_tgsi_soa_context * bld = lp_soa_context(bld_base);
+   struct gallivm_state *gallivm = bld->bld_base.base.gallivm;
+   LLVMBuilderRef builder = gallivm->builder;
+
+   printf("%s: (stub) \n",  __FUNCTION__); fflush(stdout);
+}
+
 static LLVMValueRef
 mask_vec(struct lp_build_tgsi_context *bld_base)
 {
@@ -4076,6 +4089,7 @@ lp_build_tgsi_soa(struct gallivm_state *gallivm,
    bld.bld_base.op_actions[TGSI_OPCODE_LOD].emit = lod_emit;
 
    bld.bld_base.op_actions[TGSI_OPCODE_LOAD].emit = load_emit;
+   bld.bld_base.op_actions[TGSI_OPCODE_STORE].emit = store_emit;
 
    if (gs_iface) {
       /* There's no specific value for this because it should always
